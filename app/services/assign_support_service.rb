de56fc6a9b170a2397a93223189e56328c40fdde
VuDej <drvu47@gmail.com>
2022-09-29 16:05:38 +0200

    service objects

---
+++ app/services/assign_support_service.rb
@@ -0,0 +1,17 @@
+class AssignSupportService < ApplicationService
+  attr_reader :user, :support_name
+
+  def initialize(user, support_name)
+    super
+    @user = user
+    @support_name = support_name
+  end
+
+  def call
+    support = Support.find_by name: @support_name
+    raise StandardError, "Couldn't assign a support person" unless support.present?
+
+    @user.support_id = support[:id]
+    @user.save
+  end
+end
---
+++ app/services/create_user_service.rb
@@ -0,0 +1,17 @@
+class CreateUserService < ApplicationService
+  attr_reader :name, :email, :pass
+
+  def initialize(name, email, pass)
+    super
+    @name = name
+    @email = email
+    @pass = pass
+  end
+
+  def call
+    prev_user = User.find_by email: @email
+    raise StandardError, 'User already exists' if prev_user.present?
+
+    User.create(name: @name, email: @email, pass: @pass)
+  end
+end
